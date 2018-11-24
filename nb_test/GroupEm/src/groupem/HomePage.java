1a23f7111b5d620f27f494e05e9c4dee504dd58c
walter-scott <mail.wscott@gmail.com>
2018-11-24 14:04:15 -0600

    ifughfdpiuhg

--- nb_test/GroupEm/src/groupem/HomePage.java
+++ nb_test/GroupEm/src/groupem/HomePage.java
@@ -7,6 +7,7 @@ package groupem;
 
 import static groupem.GroupEm.eventss;
 import static groupem.GroupEm.getEvent;
+import static groupem.GroupEm.hp;
 import static groupem.GroupEm.pan;
 import static groupem.GroupEm.userEvents;
 import static groupem.GroupEm.userEventNames;
@@ -55,7 +56,7 @@ public class HomePage extends javax.swing.JFrame {
         jMenuItem1 = new javax.swing.JMenuItem();
         jScrollPane1 = new javax.swing.JScrollPane();
         jList1 = new javax.swing.JList<>();
-        jButton1 = new javax.swing.JButton();
+        logout = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
 
@@ -72,7 +73,7 @@ public class HomePage extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jList1.setFont(new java.awt.Font("Heiti TC", 2, 18)); // NOI18N
+        jList1.setFont(new java.awt.Font("Helvetica", 0, 12)); // NOI18N
         jList1.setModel(new javax.swing.AbstractListModel<String>() {
             //String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
             String[] strings = userEventNames;
@@ -86,7 +87,12 @@ public class HomePage extends javax.swing.JFrame {
         });
         jScrollPane1.setViewportView(jList1);
 
-        jButton1.setText("log out");
+        logout.setText("log out");
+        logout.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                logoutActionPerformed(evt);
+            }
+        });
 
         jButton2.setText("create event");
         jButton2.addActionListener(new java.awt.event.ActionListener() {
@@ -103,7 +109,7 @@ public class HomePage extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(15, 15, 15)
-                .addComponent(jButton1)
+                .addComponent(logout)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jButton2)
                 .addGap(15, 15, 15))
@@ -119,7 +125,7 @@ public class HomePage extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1)
+                    .addComponent(logout)
                     .addComponent(jButton2))
                 .addGap(47, 47, 47)
                 .addComponent(jLabel1)
@@ -149,6 +155,13 @@ public class HomePage extends javax.swing.JFrame {
         c.setVisible(true);
         setVisible(false);
     }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void logoutActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_logoutActionPerformed
+        dispose();
+        Login l = new Login(this, true);
+        l.setVisible(true);
+        
+    }//GEN-LAST:event_logoutActionPerformed
 
 
     /**
@@ -212,7 +225,6 @@ public class HomePage extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JCheckBoxMenuItem jCheckBoxMenuItem1;
     private javax.swing.JLabel jLabel1;
@@ -222,5 +234,6 @@ public class HomePage extends javax.swing.JFrame {
     private javax.swing.JMenu jMenu3;
     private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JButton logout;
     // End of variables declaration//GEN-END:variables
 }
